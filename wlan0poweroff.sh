0b54f65c1dd097b235112555087daa8c4b855e57
Taka Kitazume <taka.kitazume@gmail.com>
2021-01-04 14:57:36 -0500

    Updating instructions to follow ~/services/rpiwlan0poweroff directory.

--- wlan0poweroff.sh
+++ wlan0poweroff.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo "Setting wlan0 power management off in 5 sec..."
-iwconfig wlan0|grep Power\ Management
+
+LOG_FILE="/var/log/rpiwlan0poweroff.log"
+echo "Setting wlan0 power management off in 5 sec..." > $LOG_FILE
+echo "Current wlan0 power management configuration" >> $LOG_FILE
+iwconfig wlan0|grep Power\ Management >> $LOG_FILE
 sleep 5
+echo "Setting wlan0 power management off..." >> $LOG_FILE
 iwconfig wlan0 power off
-iwconfig wlan0|grep Power\ Management
+iwconfig wlan0|grep Power\ Management >> $LOG_FILE
